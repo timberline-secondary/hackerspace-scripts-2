17716b1fa1cb4354ea74d908b3346d8f1a866d96
tylerecouture <tylerecouture@gmail.com>
2018-06-22 18:20:21 -0700

    omg the menu is working

--- controlpanel.py
+++ controlpanel.py
@@ -1,18 +1,46 @@
 import os
 import sys
+from importlib import import_module
 
 from scripts._utils import utils
+# from scripts import themes
 
-SCRIPTS_ROOT = os.path.join(os.path.dirname(os.path.realpath(__file__)), "scripts")
+ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
+SCRIPTS_DIR = "scripts"
 
 
-def get_menu_items(directory=None):
-    directory = os.path.join(SCRIPTS_ROOT, directory)
-    dir_contents = os.listdir(directory)
-    # remove "hidden" items beginning with _ or .
-    # https://stackoverflow.com/questions/36268749/remove-multiple-items-from-a-python-list-in-just-one-statement
-    dir_contents = [s for s in dir_contents if s[0] not in "_."]
-    return dir_contents
+def load_modules():
+    # https://stackoverflow.com/a/951678/2700631
+    module_dir = SCRIPTS_DIR
+    # scripts_dir += (os.sep + module_name) if module_name else ''
+    module_dict = {}
+    # check subfolders
+    lst = os.listdir(module_dir)
+    directories = []
+    lst = [s for s in lst if s[0] not in "_."]  # remove hidden modules
+    for d in lst:
+        s = os.path.abspath(module_dir) + os.sep + d
+        if os.path.isdir(s) and os.path.exists(s + os.sep + "__init__.py"):
+            directories.append(d)
+    # load the modules
+    for d in directories:
+        module_dict[d] = __import__(module_dir + "." + d, fromlist=["*"])
+
+    return module_dict
+
+
+def load_scripts(module):
+    scripts_dir = SCRIPTS_DIR + os.sep + module
+    scripts = os.listdir(scripts_dir)
+    scripts = [s for s in scripts if s[0] not in "_."]  # remove hidden files
+    scripts = [s[:-3] for s in scripts]  # remove '.py'
+
+    script_dict = {}
+    module_dir = SCRIPTS_DIR
+
+    for s in scripts:
+        script_dict[s] = __import__(module_dir + "." + module + "." + s, fromlist=["*"])
+    return script_dict
 
 
 def print_menu(menu_items, title):
@@ -23,30 +51,40 @@ def print_menu(menu_items, title):
         print("{}. {}".format(i, item))
     print("Ctrl+C to quit")
 
-    return input("\nChoose an item: ")
+    choice = input("\nChoose an item: ")
+
+    return menu_items[int(choice)]
 
 
-def control_panel(directory=SCRIPTS_ROOT, title="Hackerspace Control Panel"):
+def control_panel():
+
     try:
-        menu_items = get_menu_items(directory)
-        choice = int(print_menu(menu_items, title))
-        # try to dynamically import the choice as a package
-        module_name = menu_items[choice]
-        try:
-            control_panel.current_module = globals()[module_name]
-            print(control_panel.current_module)
-            control_panel(module_name, module_name)
-        except ImportError:  # not a package!
-            method = getattr(control_panel.current_module, module_name)
-            print(method)
-            print("ImportError")
+        # get modules
+        module_dict = load_modules()
+        module_choice = print_menu(list(module_dict.keys()), "Hackerspace Control Panel")
+
+        # get scripts from chosen module
+        sub_module_dict = load_scripts(module_choice)
+        sub_module_choice_str = print_menu(list(sub_module_dict.keys()), module_choice)
+        sub_module = sub_module_dict[sub_module_choice_str]
+
+        print(sub_module)
+        method = getattr(sub_module, sub_module_choice_str)
+        utils.print_heading(sub_module_choice_str)
+        method()
+
+        # import the package module
+        # print(control_panel.current_module)
+        # control_panel.current_module = import_module('scripts.'+module_name)
+        # print("current module: {}".format(control_panel.current_module))
 
     except KeyboardInterrupt:
         print("\nGoodbye")
         sys.exit(0)
 
-control_panel.current_module = None
+
+control_panel.current_module = 'scripts'
+
 
 if __name__ == '__main__':
-    # print(globals())
     control_panel()
---
+++ scripts/themes/add_new_theme.py
@@ -0,0 +1,97 @@
+import os
+import subprocess
+from urllib.parse import urlparse
+from urllib.request import urlopen
+
+import asyncssh
+
+from scripts._utils import utils
+
+
+async def run_client2():
+    # logged in user, default SSH client keys or certificates
+    async with asyncssh.connect('pi-themes', username='pi') as conn:
+        async with conn.create_process('bc') as process:
+            for op in ['2+2', '1*2*3*4', '2^32']:
+                process.stdin.write(op + '\n')
+                result = await process.stdout.readline()
+                print(op, '=', result, end='')
+
+async def run_client3():
+    async with asyncssh.connect('pi-themes', username='pi') as conn:
+        await conn.run('tail', input='1\n2\n3\n', stdout='/tmp/stdout')
+
+
+async def run_client():
+    async with asyncssh.connect('pi-themes', username='pi') as conn:
+        local_proc = subprocess.Popen(r'echo "1\n2\n3"', shell=True,
+                                      stdout=subprocess.PIPE)
+        remote_result = await conn.run('tail', stdin=local_proc.stdout)
+        print(remote_result)
+        #print(remote_result.stdout, end='')
+
+
+def add_new_theme():
+    # get and check the url of the file
+    have_good_input = False
+    mp3_url = ""
+    while not have_good_input:
+        mp3_url = input("Paste the url to the mp3 file you want to add or [q]uit: ")
+
+        if mp3_url == 'q':
+            break
+
+        # check content to ensure proper mp3 content type.
+        try:
+            with urlopen(mp3_url) as response:
+                ct = response.info().get_content_type()
+                if ct == "audio/mpeg":
+                    utils.print_styled(utils.ByteStyle.SUCCESS, "File looks good.")
+                    have_good_input = True
+                else:
+                    utils.print_styled(utils.ByteStyle.FAIL,
+                                       "Something is funky about this file.  I expected type 'audio/mpeg' but got '{}'."
+                                       " Make sure it was properly exported to an mp3.".format(ct))
+
+        except ValueError as e:
+            utils.print_styled(utils.ByteStyle.FAIL, str(e))
+
+    if have_good_input:  # then get the file number
+
+        have_good_input = False
+        while not have_good_input:
+            filename = os.path.basename(urlparse(mp3_url).path)
+            name, ext = os.path.splitext(filename)
+
+            # check if the filename is already a number, and offer to use it
+            try:
+                name = int(name)
+                good_name_already = True
+            except ValueError:
+                good_name_already = False
+
+            prompt = "What number (integers only) do you want to give it? " + ("[Enter] = {}".format(name) if good_name_already else "")
+            mp3_number = input(prompt)
+
+            try:
+                if good_name_already and mp3_number:
+                    mp3_number = name
+                else:
+                    mp3_number = int(mp3_number)
+                have_good_input = True
+            except ValueError:
+                utils.print_styled(utils.ByteStyle.FAIL, "Dude, that wasn't an integer! ")
+                have_good_input = False
+
+        filename = "{}.mp3".format(mp3_number)
+
+        print("test: {}".format(filename))
+
+        # try:
+        #     asyncio.get_event_loop().run_until_complete(run_client())
+        # except (OSError, asyncssh.Error) as exc:
+        #     sys.exit('SSH connection failed: ' + str(exc))
+
+    else:
+        print("Quitting...")
+
