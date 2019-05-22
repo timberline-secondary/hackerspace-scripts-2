c2556d4282c81f940ab468ceed91fef4a224d28f
Tseilorin <hopkins.n3@gmail.com>
2019-05-22 10:34:02 -0700

    add new image now saves previous input for ez reuse

--- scripts/TVs/add_new_image.py
+++ scripts/TVs/add_new_image.py
@@ -1,18 +1,15 @@
 import os
 from urllib.parse import urlparse
-from urllib.request import urlopen
 
 from scripts._utils import utils
 from scripts._utils.ssh import SSH
 
+#this code worked on by Nicholas (Tseilorin) Hopkins
 
-def add_new_image():
+def add_new_image(student_number=None, tv=None):
     have_good_type = False
     image_url = ""
     #gets and checks the url of the file
-
-    student_number = None
-    TV_number = None
 
     while not have_good_type:
         image_url = input("Paste the url to the image file you want to add. (q to quit): ").strip()
@@ -41,20 +38,29 @@ def add_new_image():
 
     if have_good_type: #then get file name
 
-            student_number = input("Enter Student Number\n")
+            student_number_input = input(("Enter Student Number. Default right now is {}\n").format(student_number)).strip()
+            if not student_number_input:
+                pass
+            else:
+                student_number = student_number_input
 
             image_name = None
-            name_good = input(("What is the name of this image? By default it will be {}").format(name_wo_ext))
+            name_good = input(("What is the name of this image? By default it will be {}\n").format(name_wo_ext)).strip()
             if not name_good:
                 image_name = name_wo_ext
             else:
                 image_name = name_good
 
-            tv = input("What TV are you sending this to? (Just the number)\n")
+            tv_input = input(("What TV are you sending this to? (Just the number) Default right now is {}\n").format(tv)).strip()
+            if not tv_input:
+                pass
+            else:
+                tv = tv_input
 
             filename = student_number + ".z." + image_name + extension
 
             print("Sending {} to hightower to see if file exists already with that name.".format(filename))
+
 
             filepath = "/home/pi-slideshow/tv{}/".format(tv)
             command = "wget -O /home/pi-slideshow/tv{}/{} {} && exit".format(tv, filename, image_url)
@@ -77,6 +83,7 @@ def add_new_image():
                 else:
                     print("\n(yes/no)\n")
             elif already_exists == False:
+                print(("{} was succesfully sent over to pi-tv{}").format(filename, tv))
                 pass
             else:
                 print("Something went wrong. Expected true or false but got something else")
@@ -85,7 +92,7 @@ def add_new_image():
 
             another_image = input("Would you like to add another image? ([yes]/no)")
             if not another_image or another_image == "yes":
-                add_new_image()
+                add_new_image(student_number, tv)
             elif another_image == "no":
                 pass
             else:
