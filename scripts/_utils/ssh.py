d5b5e14cbe90ccfb0df5a80b70de2d7aa1850f3f
Tseilorin <hopkins.n3@gmail.com>
2019-06-11 12:45:42 -0700

    actually deleted missile folder this time

--- scripts/_utils/ssh.py
+++ scripts/_utils/ssh.py
@@ -55,19 +55,6 @@ class SSH():
             print("SSH client not connected")
             return False
 
-        if cd:
-            transport = self.ssh_client.get_transport()
-            session = transport.open_session()
-            session.set_combine_stderr(True)
-            session.get_pty()
-
-            session.exec_command('sudo su root -c  "{}"'.format(command))
-            stdin = session.makefile("wb", -1)
-            stdout = session.makefile("rb", -1)
-
-            stdin.write(self.password + "\n")
-            stdin.flush()
-
         if sudo:
             transport = self.ssh_client.get_transport()
             session = transport.open_session()
