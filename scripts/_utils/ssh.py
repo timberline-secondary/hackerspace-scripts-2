0692b0d4a12aab292711f1246cc5bda6837371d2
tylerecouture <tylerecouture@gmail.com>
2019-06-19 16:49:55 -0700

    add paramiko shell

--- scripts/_utils/ssh.py
+++ scripts/_utils/ssh.py
@@ -1,19 +1,28 @@
 import paramiko
+import threading
 from getpass import getpass
+from .utils import print_error, print_success, print_warning, input_styled
+
 
 #this code worked on by Nicholas (Tseilorin) Hopkins
 
 class SSH():
 
-    def __init__(self, hostname, username, password=None):
+    def __init__(self, hostname, username, password=None, transport=False):
         self.hostname=hostname
         self.username=username
         self.password=password
         self.tv_turnoff_cmd = "echo standby 0 | cec-client -s -d 1"
         self.tv_turnon_cmd = "echo standby 1 | cec-client -s -d 1"
         self.client = None
+        self.transport = None
+        self.shell = None
+        self.session = None
 
         self.connect()
+        
+        if transport:
+            self.connect_transport()
 
     def __del__(self):
         self.close()
@@ -21,40 +30,74 @@ class SSH():
     def __str__(self):
         return "SSH connection with {}@{}".format(self.username, self.hostname)
 
+    def connect_transport(self):
+        # connect transport for interactive shell
+        # https://daanlenaerts.com/blog/2016/07/01/python-and-ssh-paramiko-shell/
+
+        #self.transport = paramiko.Transport((self.hostname, 22))
+        #self.transport.connect(username=self.username, password=self.password)
+
+        self.transport = self.client.get_transport()
+        self.session = self.transport.open_session()
+        self.session.set_combine_stderr(True)
+        self.session.get_pty()
+
+        thread = threading.Thread(target=self.process)
+        thread.daemon = True
+        thread.start()
+
     def connect(self):
         if self.password is None:
             self.password=getpass("password: ")
 
+        # connect ssh client
         self.client = paramiko.SSHClient()
         self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         try:
             self.client.connect(hostname=self.hostname, username=self.username, password=self.password)
 
         except paramiko.ssh_exception.BadAuthenticationType:
-            print ("\n\nConnection failed, password may be wrong or server does not allow this connection type.\n\n")
+            print_error ("\n\nConnection failed, password may be wrong or server does not allow this connection type.\n\n")
             return False
         except paramiko.ssh_exception.AuthenticationException:
-            print ("\n\nConnection failed, credentials may be wrong.\n\n")
+            print_error ("\n\nConnection failed, credentials may be wrong.\n\n")
             return False
         except paramiko.ssh_exception.BadHostKeyException:
-            print ("\n\nThe host key given by the SSH did not match what we were expecting.\n\n")
+            print_error ("\n\nThe host key given by the SSH did not match what we were expecting.\n\n")
             return False
         except paramiko.ssh_exception.ChannelException:
-            print ("\n\nThe attempt to open a new Channel failed.\n\n")
+            print_error ("\n\nThe attempt to open a new Channel failed.\n\n")
             return False
         except paramiko.ssh_exception.NoValidConnectionsError:
-            print ("\n\nMultiple connection attempts were made and none succeeded.\n\n")
+            print_error ("\n\nMultiple connection attempts were made and none succeeded.\n\n")
             return False
 
+        print_success("{} successful.".format(str(self)))
         return True
 
+    def invoke_shell(self):
+        self.shell = self.client.invoke_shell()
+
+    def send_shell_cmd(self, command):
+        if(self.shell):
+            self.shell.send(command + "\n")
+        else:
+            print_error("Attempting to send a shell command but the shell isn't opened.  Need to call invoke_shell() first.")
+
+    def open_shell(self):
+        self.invoke_shell()
+        while True:
+            command = input_styled('').strip()
+            self.send_shell_cmd(command)
+            if command == 'exit':
+                return
 
     def send_cmd(self, command, sudo=False, cd=False, print_stdout=True):
         if not self.is_connected():
             return -1
 
         if self.client == None:
-            print("SSH client not connected")
+            print_error("SSH client not connected")
             return False
 
         if sudo:
@@ -82,7 +125,10 @@ class SSH():
         return output
 
     def close(self):
-        self.client.close()
+        if(self.client != None):
+            self.client.close()
+        if(self.transport != None):
+            self.transport.close()
 
     def file_exists(self, filepath, filename):
         if not self.is_connected():
@@ -108,8 +154,23 @@ class SSH():
                 return True
             except EOFError as e:
                 # not connected
-                print(error_msg)
+                print_error(error_msg)
                 return False
 
-        print(error_msg)
+        print_error(error_msg)
         return False
+
+    # https://daanlenaerts.com/blog/2016/07/01/python-and-ssh-paramiko-shell/
+    def process(self):
+        global connection
+        while True:
+            # Print data when available
+            if self.shell != None and self.shell.recv_ready():
+                alldata = self.shell.recv(1024)
+                while self.shell.recv_ready():
+                    alldata += self.shell.recv(1024)
+                strdata = str(alldata, "utf8")
+                strdata.replace('\r', '') # remove windows carriage return character.. seems unecessary
+                print(strdata, end = "")
+                if(strdata.endswith("$ ")):
+                    print("\n$ ", end = "")
