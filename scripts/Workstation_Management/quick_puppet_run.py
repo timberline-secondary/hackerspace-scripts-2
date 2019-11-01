a1b935ddd7a29563a1096f529c98575c694257a7
tylerecouture <tylerecouture@gmail.com>
2019-11-01 15:00:28 -0700

    quick puppet loop for mulitple computers

--- scripts/Workstation_Management/quick_puppet_run.py
+++ scripts/Workstation_Management/quick_puppet_run.py
@@ -5,6 +5,12 @@ from scripts.Workstation_Management.puppet_run import puppet_run
 
 def quick_puppet_run(auto_fix_certificates=False, computer_number=None):
     password = getpass("Enter the admin password: ")
-    number = utils.input_styled("Enter the computer number (tbl-hackerspace-#-s): ")
+    numbers = utils.input_styled("Enter the computer numbers, seperated by spaces (where # is from hostname tbl-h10-#-s e.g: 2 15 30): ")
 
-    return puppet_run(number, password, auto_fix_certificates=True, )
+    num_list = numbers.split()
+
+    for num in num_list:
+        utils.print_warning("Trying computer #{}...".format(num))
+        puppet_run(num, password, auto_fix_certificates=True)
+    
+    return
