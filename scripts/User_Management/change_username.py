b713a63cb7a4e81de8a7474ea0d8d468073d8f50
tylerecouture <tylerecouture@gmail.com>
2020-06-04 18:42:24 +0000

    change homedir in ldap

--- scripts/User_Management/change_username.py
+++ scripts/User_Management/change_username.py
@@ -1,7 +1,3 @@
-import os
-import re
-from urllib.parse import urlparse
-
 from scripts._utils import utils
 from scripts._utils.ssh import SSH
 
@@ -13,7 +9,8 @@ auth_hostname = 'lannister'
 file_hostname = 'tyrell'
 username = 'hackerspace_admin'
 
-def change_username ():
+
+def change_username():
 
     utils.print_warning("\nMake sure the student is logged out before making this change!\n")
 
@@ -37,13 +34,15 @@ def change_username ():
     command = "sudo ldaprenameuser {} {}".format(current_username, new_username)
     ssh_connection.send_cmd(command, sudo=True)
     ssh_connection.close()
-    
+
     utils.print_warning("Now gonna change the name of their home directory to match the new username")
-    
+
     ssh_connection = SSH(file_hostname, username, password)
     command = "mv /nfshome/{} /nfshome/{}".format(current_username, new_username)
     ssh_connection.send_cmd(command, sudo=True)
     ssh_connection.close()
     utils.print_success("Done!")
 
-    # utils.print_warning("Now gonna tell LDAP where the new home directory is")
+    utils.print_warning("Now gonna tell LDAP where the new home directory is")
+
+    user_utils.modify_user(new_username, {'homeDirectory': f'/home/{new_username}'}, password)
