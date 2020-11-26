73891ab9fcf570c0f7b74ffbee674e431b7839a4
tylerecouture <tylerecouture@gmail.com>
2020-11-26 13:54:00 -0800

    puppet lock WIP

--- scripts/Workstation_Management/_remove_puppet_lock.py
+++ scripts/Workstation_Management/_remove_puppet_lock.py
@@ -9,6 +9,8 @@ def remove_puppet_lock(ssh_connection, password=None, ):
     # check when last puppet run was by reading motd
     motd = ssh_connection.send_cmd('cat /etc/motd')
 
+    print(motd)
+
     pattern = r"Last run: (.*)$"
 
     time_str = re.search(pattern, motd)
