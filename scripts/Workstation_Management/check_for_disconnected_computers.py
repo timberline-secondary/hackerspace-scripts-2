bcf9b4fb1d555c783cb804dfe65d76993c68fae2
tylerecouture <tylerecouture@gmail.com>
2021-04-23 11:36:19 -0700

    fix check all connected workstations

--- scripts/Workstation_Management/check_for_disconnected_computers.py
+++ scripts/Workstation_Management/check_for_disconnected_computers.py
@@ -1,4 +1,4 @@
-import subprocess
+# import subprocess
 
 from scripts._utils import utils
 
@@ -12,18 +12,26 @@ def check_for_disconnected_computers(password=None):
 
     computer_numbers = range(FIRST, LAST + 1)
 
+    for i in computer_numbers:
+        hostname = f'tbl-h10-{i}'
+        if utils.host_exists(hostname, verbose=False):
+            utils.print_success(f"{hostname} found.")
+        else:
+            utils.print_error(f"{hostname} missing.")
+
+    ## This doesn't work with new DNS Setup.  Just ping instead.
     # https://itsfoss.com/how-to-find-what-devices-are-connected-to-network-in-ubuntu/
     # cmd = ["nmap -sn 192.168.43.0/24 | grep 'tbl-h10' | sort"]
     # output = subprocess.run(cmd, shell=True, capture_output=True, text=True).stdout
-    cmd = ["nmap", "-sn", "192.168.43.0/24"]
-    output = subprocess.run(cmd, capture_output=True, text=True).stdout
+    # cmd = ["nmap", "-sn", "192.168.43.0/24"]
+    # output = subprocess.run(cmd, capture_output=True, text=True).stdout
 
-    found_at_least_one = False
-    for i in computer_numbers:
-        host = f'tbl-h10-{i}'
-        if host not in output:
-            utils.print_warning(f"Computer {host} was not found on the network.")
-            found_at_least_one = True
+    # found_at_least_one = False
+    # for i in computer_numbers:
+    #     host = f'tbl-h10-{i}'
+    #     if host not in output:
+    #         utils.print_warning(f"Computer {host} was not found on the network.")
+    #         found_at_least_one = True
 
-    if not found_at_least_one:
-        utils.print_success(f"All computers from tbl-h10-{FIRST} to tbl-h10-{LAST} were found on the network.")
+    # if not found_at_least_one:
+    #     utils.print_success(f"All computers from tbl-h10-{FIRST} to tbl-h10-{LAST} were found on the network.")
