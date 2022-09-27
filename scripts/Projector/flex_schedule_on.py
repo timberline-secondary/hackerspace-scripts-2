a260907694b1135365313506365b93728a07207b
matt <matt@dont-ping.me>
2022-09-27 16:40:04 -0700

    feat(projector): add flex toggling
    Added:
    - new command set_flex_schedule
        - This command will change the current schedule that runs on flex days
    - Added new dependency to requirements
    
    Fixes:
    - Fixed flex_schedule_off and flex_schedule_on
        - Change the path to toggle scripts

--- scripts/Projector/flex_schedule_on.py
+++ scripts/Projector/flex_schedule_on.py
@@ -5,6 +5,6 @@ hostname = "pi-projector"
 
 def flex_schedule_on():
     ssh_connection = ssh.SSH(hostname, pi.username, pi.password)
-    ssh_connection.send_cmd("touch /tmp/flex")
+    ssh_connection.send_cmd("touch /home/pi/clock/public/flex-toggle")
     print("Done.")
     ssh_connection.close()
---
+++ scripts/Projector/set_flex_schedule.py
@@ -0,0 +1,64 @@
+import json
+import re
+
+from sty import fg
+
+from scripts._utils import ssh, pi, utils
+
+hostname = "pi-projector"
+
+
+def print_schedule(data, index, i=0):
+    for block in data["schedule"][index]["values"]:
+        i += 1
+        rgb = re.findall(r'\d+', block['colour'])
+        print(
+            f"{i}: {block['text']}, {block['start']} - {block['end']} & {fg(rgb[0], rgb[1], rgb[2]) if rgb else fg(128, 128, 128)}{block['colour']}{fg.rs}")
+
+
+def set_flex_schedule():
+    # Connect to the clock
+    ssh_connection = ssh.SSH(hostname, pi.username, pi.password)
+    ssh_connection.connect_sftp()
+    ssh_connection.get_file("/home/pi/clock/components/schedule.json", "/tmp/schedule.json")  # download the schedule file locally
+
+    def select_schedule():
+        # get user input on the flex index
+        flex_input = utils.input_styled("Which index should the flex schedule be? (int) > ")
+        if flex_input.lower() == 'q':  # q chosen as option; quit.
+            return True
+        elif not flex_input.isdigit():  # Flex input was not a number.
+            print("Not valid index")
+            return False
+
+        # get only number if number in string
+        index = [int(s) for s in flex_input.split() if s.isdigit()][0]
+
+        # open the schedule.json
+        f = open("/tmp/schedule.json", "r")
+        data = json.loads(f.read())
+        try:
+            print("\n" + data["schedule"][index]["description"] + "\n")  # Print the description of the schedule
+        except IndexError:
+            print("Index out of range.")  # The flex inout number is out of range in teh schedule file
+            return False
+
+        # Print the blocks & times in the schedule
+        print_schedule(data, index)
+
+        confirmation = utils.input_styled("\nIs this the correct schedule? [y/N] (q to quit): \n")
+        if confirmation and confirmation[0].lower() == "y":  # update clock file & exit loop
+            ssh_connection.send_cmd("touch /home/pi/clock/public/flex-index >> echo '1' > /home/pi/clock/public/flex-index")
+            return True
+        elif confirmation == "q":  # Quit loop
+            return True
+        else:  # Continue while loop
+            return False
+
+    # make sure a schedule is chosen or quit is called; do not exit on incorrect input
+    while True:
+        if select_schedule():
+            break
+
+    print("\nDone.")
+    ssh_connection.close()
