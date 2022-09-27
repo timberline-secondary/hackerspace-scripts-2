43142675a3900c1fc130a4a85219f6b39dca2486
matt <matt@dont-ping.me>
2022-09-27 16:23:11 -0700

    revert(clock): revert clock changes from 99e9faa8

--- scripts/Projector/flex_schedule_off.py
+++ scripts/Projector/flex_schedule_off.py
@@ -5,6 +5,6 @@ hostname = "pi-projector"
 
 def flex_schedule_off():
     ssh_connection = ssh.SSH(hostname, pi.username, pi.password)
-    ssh_connection.send_cmd("rm /home/pi/clock/public/flex-toggle")
+    ssh_connection.send_cmd("rm /tmp/flex")
     print("Done.")
     ssh_connection.close()
--- scripts/Projector/flex_schedule_on.py
+++ scripts/Projector/flex_schedule_on.py
@@ -5,6 +5,6 @@ hostname = "pi-projector"
 
 def flex_schedule_on():
     ssh_connection = ssh.SSH(hostname, pi.username, pi.password)
-    ssh_connection.send_cmd("touch /home/pi/clock/public/flex-toggle")
+    ssh_connection.send_cmd("touch /tmp/flex")
     print("Done.")
     ssh_connection.close()
