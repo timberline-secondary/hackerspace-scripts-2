6612eae166341b92fd6439632f661f323301261d
matt <matt@dont-ping.me>
2022-10-12 11:12:46 -0700

    fix(grade_9): separate for loop & less ambiguity

--- scripts/Miscellaneous/grade_9_mode.py
+++ scripts/Miscellaneous/grade_9_mode.py
@@ -7,35 +7,34 @@ TV_OFF_CMD = "echo standby 0 | cec-client -s -d 1"
 TV_ON_CMD = "echo on 0 | cec-client -s -d 1"
 
 
-def engage():
-    ssh_connection = ssh.SSH(host_themes, pi.username, pi.password)
-    ssh_connection.send_cmd("amixer sset 'Headphone',0 0%")
-
+def send_tvs_command(command):
     for tv_number in tvs:
         hostname = "pi-tv{}".format(tv_number)
         ssh_connection = ssh.SSH(hostname, pi.username, pi.password)
 
-        ssh_connection.send_cmd(TV_OFF_CMD)
+        ssh_connection.send_cmd(command)
         ssh_connection.close()
+
+
+def engage():
+    ssh_connection = ssh.SSH(host_themes, pi.username, pi.password)
+    ssh_connection.send_cmd("amixer sset 'Headphone',0 0%")
+
+    send_tvs_command(TV_OFF_CMD)
 
 
 def disengage():
     ssh_connection = ssh.SSH(host_themes, pi.username, pi.password)
     ssh_connection.send_cmd("amixer sset 'Headphone',0 100%")
 
-    for tv_number in tvs:
-        hostname = "pi-tv{}".format(tv_number)
-        ssh_connection = ssh.SSH(hostname, pi.username, pi.password)
-
-        ssh_connection.send_cmd(TV_ON_CMD)
-        ssh_connection.close()
+    send_tvs_command(TV_ON_CMD)
 
 
 def grade_9_mode():
-    confirmation = utils.input_styled("Do you want to engage grade 9s? [y/N] (q to quit): \n")
-    if confirmation and confirmation[0].lower() == "y":
+    confirmation = utils.input_styled("Do you want to engage grade 9s? [e]ngage/[D]isengage (q to quit): \n")
+    if confirmation and confirmation[0].lower() == "e":
         engage()
-    elif confirmation == "q":
+    elif confirmation and confirmation[0].lower() == "q":
         return False
     else:
         disengage()
