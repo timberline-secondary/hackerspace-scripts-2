558b758c9ac5709a5c86f9c8966913fd5344cc02
matt <matt@dont-ping.me>
2022-10-13 08:13:10 -0700

    feat(grade_9): add threading to speed up process

--- scripts/Miscellaneous/grade_9_mode.py
+++ scripts/Miscellaneous/grade_9_mode.py
@@ -1,3 +1,5 @@
+import threading
+
 from scripts._utils import ssh, pi, utils
 
 host_themes = "pi-themes"
@@ -7,13 +9,23 @@ TV_OFF_CMD = "echo standby 0 | cec-client -s -d 1"
 TV_ON_CMD = "echo on 0 | cec-client -s -d 1"
 
 
-def send_tvs_command(command):
-    for tv_number in tvs:
-        hostname = "pi-tv{}".format(tv_number)
-        ssh_connection = ssh.SSH(hostname, pi.username, pi.password)
+def tv_thread(command, tv_number):
+    hostname = "pi-tv{}".format(tv_number)
+    ssh_connection = ssh.SSH(hostname, pi.username, pi.password)
 
-        ssh_connection.send_cmd(command)
-        ssh_connection.close()
+    ssh_connection.send_cmd(command)
+    ssh_connection.close()
+
+
+def send_tvs_command(command):
+    global thread
+
+    for tv_number in tvs:
+        thread = threading.Thread(target=tv_thread, args=(command, tv_number,))
+        thread.start()
+
+    # wait for threads to finish
+    thread.join()
 
 
 def engage():
