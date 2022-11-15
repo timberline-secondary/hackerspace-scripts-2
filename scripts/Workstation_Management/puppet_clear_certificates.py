96809ff5e9b0141223cac4488aecc07cdf825713
matt <matt@dont-ping.me>
2022-11-15 08:44:35 -0800

    refactor(puppet_cert): refactor puppet cert command
    CHANGES:
    - Allowing for space-seperated computer numbers (like most other commands)

--- scripts/Workstation_Management/puppet_clear_certificates.py
+++ scripts/Workstation_Management/puppet_clear_certificates.py
@@ -5,7 +5,28 @@ from scripts._utils.ssh import SSH
 
 puppet_host = 'puppet'
 username = 'hackerspace_admin'
-computer_host = None
+
+
+def puppet_command(computer_number, password):
+    computer_host = utils.get_valid_hostname(computer_number)
+
+    if computer_host is None:
+        return
+
+    remove_server_cert_cmd = "/opt/puppetlabs/bin/puppetserver ca clean --certname {}.hackerspace.tbl".format(computer_host)
+
+    # now that we know we have a connected computer, ssh into it and try to run command
+    ssh_connection_puppet = SSH(puppet_host, username, password)
+
+    if not ssh_connection_puppet.is_connected():
+        utils.print_warning("\nComputer is online, but can't connect. Maybe it's mining?\n")
+        return False
+
+    # run command
+    ssh_connection_puppet.send_cmd(remove_server_cert_cmd, sudo=True)
+    ssh_connection_puppet.close()
+
+    utils.print_warning("Ok, I tried to remove the old certificates from the puppet server.")
 
 
 def puppet_clear_certificates(hostname=None, password=None):
@@ -13,16 +34,22 @@ def puppet_clear_certificates(hostname=None, password=None):
         password = getpass("Enter the admin password: ")
 
     if hostname is None:
-        hostname = utils.input_styled("Which computer? (e.g. 'tbl-h10-12', or '192.168.3.125' or [q]uit) ")
+        hostname = utils.input_styled("Enter the computer numbers, seperated by spaces \n"
+                                     "(where # is from hostname tbl-h10-#-s e.g: 2 15 30)\n"
+                                     " or 'all' to run on all computers or [q]uit: ")
 
-        if hostname == 'q':
+        if hostname == "q":
             print("Quitting this.")
             return None
 
-    remove_server_cert_cmd = "/opt/puppetlabs/bin/puppetserver ca clean --certname {}.hackerspace.tbl".format(hostname)
+        num_list = hostname.split()
 
-    ssh_connection_puppet = SSH(puppet_host, username, password)
-    ssh_connection_puppet.send_cmd(remove_server_cert_cmd, sudo=True)
-    ssh_connection_puppet.close()
+        if num_list == "":
+            return
 
-    utils.print_warning("Ok, I tried to remov the old certificates from the puppet server.")
+        if num_list[0] == "all":
+            num_list = [f"{i}" for i in range(0, 32)]  # list of strings.  0 will cause problem if int instead of str
+
+        for num in num_list:
+            utils.print_warning("Trying computer #{}...".format(num))
+            puppet_command(num, password)
